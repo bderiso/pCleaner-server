496820928b1ffa3a0ae44590540c642011b132dc
avtech-SDC <avtech-1@avtech-SDC.local>
2018-05-25 18:36:07 -0500

    Removing a lot of server version specific logic

--- audio-processing.sh
+++ audio-processing.sh
@@ -45,45 +45,21 @@ for INFILE in $(find "$IN_DIR"/ -path "$IN_DIR"/archive -prune -o -type f -print
     exec "$0"
   fi
  
-    # file has been closed, process it
-    FEED_NAME=$(echo "$INFILE" | cut -d "/" -f5)
-    EPISODE_TITLE=$(~pcc/.local/bin/greg check -f $FEED_NAME | head -1 | sed "s/^0: //")
-    OUTFILE_PATH="$OUT_DIR"/"$FEED_NAME"
-    OUTFILE_NAME=$(echo "$INFILE" | cut -d "/" -f6 | cut -d "." -f1)
-    AD="0,0.050"
-    T=-$(sox -t "$INFILE_FORMAT" "$INFILE" -n stats 2> >(fgrep 'RMS lev dB') | cut -d '-' -f2 | cut -d ' ' -f1)
-    R=$(echo "$T" / 3 | bc)
-    F=$(echo "$T" \* 3.5 | bc)
-    #TIER=basic
-    TIER=premium
-    #AUDIO_FX_SETTINGS=$(cat ~pcc/pCleaner/sox-"$TIER"-settings)
-    if [ "$TIER" = premium ]; then
-#        OUTFILE_FORMAT_LIST='mp3
-#flac
-#wav'
-          OUTFILE_FORMAT_LIST='wav'
-          else
-          OUTFILE_FORMAT_LIST='mp3'
-    fi
+  # file has been closed, process it
+  OUTFILE_NAME=$(echo "$INFILE" | cut -d "/" -f6 | cut -d "." -f1)
+  AD="0,0.050"
+  T=-$(sox -t "$INFILE_FORMAT" "$INFILE" -n stats 2> >(fgrep 'RMS lev dB') | cut -d '-' -f2 | cut -d ' ' -f1)
+  R=$(echo "$T" / 3 | bc)
+  F=$(echo "$T" \* 3.5 | bc)
+  OUTFILE_FORMAT_LIST='wav'
 
-    # Automatic handling of output formats from a space delimited list
-    if [ ! -e "$OUTFILE_PATH" ]; then
-      mkdir -p "$OUTFILE_PATH"
-    fi
+  # Automatic handling of output formats from a space delimited list
+  for OUTFILE_FORMAT in $OUTFILE_FORMAT_LIST; do
+    OUTFILE="$OUTFILE_PATH"/"$OUTFILE_NAME"."$OUTFILE_FORMAT"
+  done
 
-    for OUTFILE_FORMAT in $OUTFILE_FORMAT_LIST; do
-      OUTFILE="$OUTFILE_PATH"/"$OUTFILE_NAME"."$OUTFILE_FORMAT"
+  sox -V --no-clobber -t "$INFILE_FORMAT" "$INFILE" "$OUTFILE" highpass 20 lowpass 20k mcompand "$AD 6:$T,$R -6 $F" 160 "$AD 6:$T,$R -6 $F" 1000 "$AD 6:$T,$R -6 $F" 8000 "$AD 6:$T,$R -6 $F" gain -n -2
 
-      AUDIO_FX=$(echo eval $(cat ~pcc/pCleaner/sox-"$TIER"-settings))
-
-      #sox -V --no-clobber -t "$INFILE_FORMAT" "$INFILE" "$OUTFILE" "$AUDIO_FX"
-      if [ "$TIER" = premium ]; then
-        sox -V --no-clobber -t "$INFILE_FORMAT" "$INFILE" "$OUTFILE" highpass 20 lowpass 20k mcompand "$AD 6:$T,$R -6 $F" 160 "$AD 6:$T,$R -6 $F" 1000 "$AD 6:$T,$R -6 $F" 8000 "$AD 6:$T,$R -6 $F" gain -n -2
-      else
-        sox -V --no-clobber -t "$INFILE_FORMAT" "$INFILE" "$OUTFILE" $AD 6:$T,$R -6 $F gain -n -2
-      fi
-
-    done
 
   # Prevent future runs against the same file
   rsync --remove-source-files "$INFILE" "$IN_DIR"/archive/
