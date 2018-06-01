025f781f2754de93480d152bfd16b1d2dd021290
Brandon L. Deriso <brandon@Brandons-MacBook-Pro-9.local>
2018-06-01 16:41:23 -0500

    Adding feed specific path to this file for ease of maintainence for the main script

--- feed-processing.sh
+++ feed-processing.sh
@@ -1,5 +1,12 @@
 EPISODE_TITLE=$(~pcc/.local/bin/greg check -f $FEED_NAME | head -1 | sed "s/^0: //")
 OUTFILE_NAME=$(printf "$INFILE" | awk -F/ '{print $NF}' | cut -d "." -f 1)
+FEED_PATH"$OUT_DIR"/"$FEED_NAME"
+
+# Check that FEED_PATH exists; if not then make it
+if [ ! -e "$FEED_PATH" ]; then
+  echo "Creating the input directory: $FEED_PATH"
+  mkdir -p "$FEED_PATH"
+fi
 
 echo "$(date -u):"
 
@@ -7,7 +14,7 @@ echo "$(date -u):"
 ENCLOSURE_TYPE=$(if [ "$OUTFILE_FORMAT" = mp3 ]; then echo 'audio/mpeg'; elif [ "$OUTFILE_FORMAT" = wav ]; then echo 'audio/x-wav'; fi)
 OUTFILE_LENGTH=$(wc -c < "$OUTFILE")
 OUTFILE_LINK=http://PodcastCleaner.com/feeds/"$FEED_NAME"/"$OUTFILE_NAME"."$OUTFILE_FORMAT"
-FEED_RSS="$OUTFILE_PATH"/"$OUTFILE_FORMAT"-feed.rss
+FEED_RSS="$FEED_PATH"/"$OUTFILE_FORMAT"-feed.rss
 
 RSS_ITEM=$(cat <<EOF
 \
